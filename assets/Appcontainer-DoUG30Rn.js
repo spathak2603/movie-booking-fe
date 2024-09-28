e70ec69b1116bd4875ca1629bdca190d9c1b7ebb
spathak2603 <shubhampathak2603@gmail.com>
2024-09-28 17:04:24 +0530

    Updates

---
+++ assets/Appcontainer-DoUG30Rn.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DAsIOk7P.js";const a=({children:t})=>r.jsx("div",{className:"container-fluid",children:t});export{a as default};
---
+++ assets/index-CrhMS32U.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DAsIOk7P.js";const s=()=>r.jsx("div",{children:"User profile"});export{s as default};
